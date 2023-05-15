4b904948ca612c2f1713cd346356923bf24993e3
Susie Gordon <susieyoungchoi@gmail.com>
2023-05-15 10:02:42 -0400

    Commit 1 - I added my first element and it was fun

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -2,3 +2,19 @@
 //     console.log('oops! I still have to link my jQuery properly!');
 // } else {console.log('I did it! I linked jQuery and this js file!')};
 
+
+//////// YEAR 1 ////////
+
+$(() => {
+    const $body = $("body")
+	$body.append($("<div>").attr("id", "container"))
+    const $container = $("#container")
+    // console.log($container)
+
+    $container.append($("<h1>").text("Hogwarts"))   
+    const $h1 = $("h1")
+    // console.log($h1) 
+
+});
+
+
