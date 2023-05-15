2ea6a848e85eb66bd061da7399b8205e5908fa38
Susie Gordon <susieyoungchoi@gmail.com>
2023-05-15 13:08:37 -0400

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -23,17 +23,52 @@ $(() => {
 
 //////// YEAR 3 ////////
 
-   $container.append($("<ul>").attr("storage", "trunk"))
-   const $ul = $("ul")
+    $container.append($("<ul>").attr("storage", "trunk"))
+    const $ul = $("ul")
 
-   $ul.append($("<li>").text("butter beer"))
-   $ul.append($("<li>").addClass("secret").text("invisibility cloak"))
-   $ul.append($("<li>").addClass("secret").text("magic map"))
-   $ul.append($("<li>").addClass("secret").text("time turner"))
-   $ul.append($("<li>").addClass("dog").text("leash"))
-   $ul.append($("<li>").text("Bertie Bott's Every Flavor [Jelly] Bean"))
-   const $lis = $("li")
+    $ul.append($("<li>").text("butter beer"))
+    $ul.append($("<li>").addClass("secret").text("invisibility cloak"))
+    $ul.append($("<li>").addClass("secret").text("magic map"))
+    $ul.append($("<li>").addClass("secret").text("time turner"))
+    $ul.append($("<li>").addClass("dog").text("leash"))
+    $ul.append($("<li>").text("Bertie Bott's Every Flavor [Jelly] Bean"))
+    const $lis = $("li")
 //    console.log($lis)
+
+//////// YEAR 4 ////////
+
+    $container.append($("<h5>").text("Spring 2017"))
+    const $h5 = $("h5")
+
+    $container.append($("<table>"))
+    const $table = $("table")
+
+    $table.append($("<thead>"))
+    const $thead = $("thead")
+
+    $thead.append($("<th>").text("Day"))
+    $thead.append($("<th>").text("Classes"))
+    const $th = $("th")
+
+    $table.append($("<tr>").addClass("1").append($("<td>").text("Monday")))
+    const $tr1 = $(".1")
+    $tr1.append($("<td>").text("Herbology, Divination"))
+
+    $table.append($("<tr>").addClass("2").append($("<td>").text("Tuesday")))
+    const $tr2 = $(".2")
+    $tr2.append($("<td>").text("History of Magic, Charms"))
+
+    $table.append($("<tr>").addClass("3").append($("<td>").text("Wednesday")))
+    const $tr3 = $(".3")
+    $tr3.append($("<td>").text("Potions, Transfiguration"))
+
+    $table.append($("<tr>").addClass("4").append($("<td>").text("Thursday")))
+    const $tr4 = $(".4")
+    $tr4.append($("<td>").text("Defense Against the Dark Arts"))
+
+    $table.append($("<tr>").addClass("5").append($("<td>").text("Friday")))
+    const $tr5 = $(".5")
+    $tr5.append($("<td>").text("Quidditch practice"))
 
 
 });
